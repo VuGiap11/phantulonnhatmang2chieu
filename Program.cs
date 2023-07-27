5f6263c4703afc9921e852ecb32323e9c1d9d9ef
vu giap <vudinhgiap11@gmail.com>
2023-07-27 15:02:50 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timphantulonnhatmang2chieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding=Encoding.UTF8;
+            {
+                int[,] arr = {
+            { 1, 3, 5 },
+            { 7, 2, 4 },
+            { 9, 6, 8 }
+        };
+
+                int maxElement = FindMaxElement(arr);
+                Console.WriteLine("Phần tử lớn nhất trong mảng là: " + maxElement);
+            }
+        }
+        static int FindMaxElement(int[,] arr)
+        {
+            int maxElement = arr[0, 0];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > maxElement)
+                    {
+                        maxElement = arr[i, j];
+                    }
+                }
+            }
+            return maxElement;
+        }
+    }
+}
